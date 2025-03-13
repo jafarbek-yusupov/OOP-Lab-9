19732329201a21730c5fad0e80f2511ddf6224b8
j.yusupov <j.yusupov@newuu.uz>
2025-03-13 11:48:04 +0500

    // head->prev = nnode; -- crashes if head is nullptr

--- List.h
+++ List.h
@@ -4,17 +4,26 @@
 template<typename T>
 class List {
     class Node {
-        public: T val; Node * next; Node * prev; Node(T val = NULL){ this->val=val; next=nullptr; prev=nullptr; }
+        public: T val; Node * next; Node * prev; Node(T val = T()) { this->val = val; next = nullptr; prev = nullptr; }
     };
     Node* head; Node* tail;
     public:
     List(): head(nullptr), tail(nullptr){}
-    void addFront(T val){ Node* nnode = new Node(val); nnode->next=head; head->prev = nnode; head=nnode; }
-    void addTail(T val){ Node* nnode = new Node(val); nnode->prev=tail; tail->next = nnode; tail=nnode; }
+    void addFront(T val) {
+       // head->prev = nnode; -- crashes if head is nullptr
+        Node* nnode = new Node(val);
+        if(!head){ head = tail = nnode;}
+        else{ nnode->next = head; head->prev = nnode; head = nnode; }
+    }
+    void addTail(T val) {
+        Node* nnode = new Node(val);
+        if(!tail){ head = tail = nnode;}
+        else{ nnode->prev = tail; tail->next = nnode; tail = nnode;}
+    }
     T getFront(){  if(head == nullptr){ throw std::runtime_error("List is empty. Cannot get front.");} return head->val; }
     T getTail(){  if (tail == nullptr){ throw std::runtime_error("List is empty. Cannot get tail.");} return tail->val; }
-    void displayForwards(){ while(head != nullptr){ std::cout << getFront() << " -> "; head = head->next; } std::cout << "NULL\n";}
-    void displayBackwards(){ /*Node* tmp = tail;*/ while(tail != nullptr){ std::cout << getTail() << " -> "; tail = tail->prev; } std::cout << "NULL\n";}
+    void displayForward(){ Node* tmp = head; while(tmp != nullptr){ std::cout << getFront() << " -> "; tmp = tmp->next; } std::cout << "NULL\n";}
+    void displayTail(){ Node* tmp = tail; while(tmp != nullptr){ std::cout << getTail() << " -> "; tmp = tmp->prev; } std::cout << "NULL\n";}
 };
 
 #endif
